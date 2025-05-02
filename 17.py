b5eae52da3efbbc43186692768885474c11b7feb
BakdauletNN <bakdauletrysbekov2007@gmail.com>
2025-05-02 15:48:54 +0600

    task

--- 17.py
+++ 17.py
@@ -1,36 +1,29 @@
-from typing import List
-
-
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        letters = {
-            "1": '',
-            "2": ['a' ,'b', 'c'],
-            "3": ['d', 'e', 'f'],
-            "4":['g', 'h', 'i'],
-            "5":['j', 'k', 'l'],
-            "6":['m', 'n', 'o'],
-            "7":['p', 'q', 'r', 's'],
-            "8":['t', 'u', 'v'],
-            "9":['w', 'x', 'y', 'z']
-        }
-        s = []
-        idx = 0
-        string = ""
-        r = []
-        if len(digits) == 1:
-            return letters[digits]
-        if len(digits) == 0:
+        if not digits:
             return []
-        for i in digits:
-            if i in letters.keys():
-                s.append(letters[i])
+        
+        letters = {
+            "2": ['a', 'b', 'c'],
+            "3": ['d', 'e', 'f'],
+            "4": ['g', 'h', 'i'],
+            "5": ['j', 'k', 'l'],
+            "6": ['m', 'n', 'o'],
+            "7": ['p', 'q', 'r', 's'],
+            "8": ['t', 'u', 'v'],
+            "9": ['w', 'x', 'y', 'z']
+        }
+        
+        result = []
+        
+        def backtrack(index, current_combination):
+            if index == len(digits):
+                result.append(current_combination)
+                return
+            current_digit = digits[index]
+            for letter in letters[current_digit]:
+                backtrack(index + 1, current_combination + letter)
+        
+        backtrack(0, "")
+        return result
 
-        for j in range(0,len(s)):
-            for k in range(j):
-                string += s[idx][idx]
-                string += s[j][k]
-                string += s[j +1][k]
-                r.append(string)
-        return r
-print(Solution().letterCombinations(digits="234"))
