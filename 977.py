c3b9471882a3841340459b72c46ca8d8725e09b0
BakdauletNN <bakdauletrysbekov2007@gmail.com>
2025-05-02 23:55:14 +0600

    task

--- 977.py
+++ 977.py
@@ -1,24 +1,20 @@
-from typing import List
-
-
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
         left_side = 0
         right_side = len(nums) - 1
-        res = [0] * len(nums)
-        idx = len(nums) - 1
+        result = [0] * len(nums)
+        index = right_side
+        
         while left_side <= right_side:
-            if nums[left_side] ** 2 < nums[right_side] **2:
-                res[idx] = nums[right_side] ** 2
-                right_side -= 1
-                
-            else:
-                res[idx] = nums[left_side] ** 2
+            left_sq = nums[left_side] ** 2
+            right_sq = nums[right_side] ** 2
+            
+            if left_sq >= right_sq:
+                result[index] = left_sq
                 left_side += 1
-            idx -= 1
-                
-        return res
-
-print(Solution().sortedSquares(nums = [-4,-1,0,3,10]))
-
-
+            else:
+                result[index] = right_sq
+                right_side -= 1
+            index -= 1
+        
+        return result
