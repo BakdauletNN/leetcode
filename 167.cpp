6c54b16585b0163041a133399c1ef204335760d3
BakdauletNN <bakdauletrysbekov2007@gmail.com>
2025-05-03 18:16:32 +0600

    Update 167.cpp

--- 167.cpp
+++ 167.cpp
@@ -1,19 +1,21 @@
-from typing import List
+class Solution {
+    public:
+        vector<int> twoSum(vector<int>& numbers, int target) {
+            int left = 0;
+            int right = numbers.size() - 1;
+            while (left < right){
+                int cur_sum = numbers[left] + numbers[right];
+                if (cur_sum == target){
+                    return {left + 1, right + 1};
+                }
+                else if(cur_sum < target){
+                    left ++;
+                }
+                else{
+                    right --;
+                }
 
-class Solution:
-    def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        left, right = 0, len(numbers) - 1
-        
-        while left < right:
-            current_sum = numbers[left] + numbers[right]
-            if current_sum == target:
-                return [left + 1, right + 1]
-            elif current_sum < target:
-                left += 1
-            else:
-                right -= 1
-        
-        return [-1, -1]  # Если решение не найдено (но по условию оно всегда есть)
-
-
-print(Solution().twoSum(numbers = [1,2,3,4,4,9,56,90], target = 8))
+            }
+            return {};
+        }
+    };
