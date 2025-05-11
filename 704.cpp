12ede1ba10299d4112b98d9524987beb879f3364
BakdauletNN <bakdauletrysbekov2007@gmail.com>
2025-05-12 02:31:06 +0600

    task

--- 704.cpp
+++ 704.cpp
@@ -1,19 +1,20 @@
-from typing import List
-
-
-class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        left, right = 0, len(nums) - 1
-        while left<= right:
-            middle = (left + right) // 2
-            if nums[middle] > target:
-                right = middle - 1
-            if nums[middle] < target:
-                left = middle + 1
-            if nums[middle] == target:
-                return middle
-        return -1
-
-
-
-print(Solution().search(nums = [-1,0,5], target = -1))
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+        
+        while (left <= right) {
+            int middle = left + (right - left) / 2;
+            if (nums[middle] == target) {
+                return middle;
+            } else if (nums[middle] < target) {
+                left = middle + 1;
+            } else {
+                right = middle - 1;
+            }
+        }
+        
+        return -1;
+    }
+};
