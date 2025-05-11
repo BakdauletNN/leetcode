4cf86bb21ac4857b4b9d97e9a180f50698a0e76c
BakdauletNN <bakdauletrysbekov2007@gmail.com>
2025-05-12 02:29:24 +0600

    task

--- 26.cpp
+++ 26.cpp
@@ -1,15 +1,14 @@
-from typing import List
-
-
-class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
-        k = 0
-        for i in range(1,len(nums)):
-            if nums[i] == nums[k]:
-                nums[i] = nums[k]
-            k += 1
-        return k
-print(Solution().removeDuplicates(nums = [0,0,1,1,1,2,2,3,3,4]))
-
-
-
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int k = 1;
+        for (int i = 1; i < nums.size(); i++){
+            if (nums[i] != nums[i - 1]){
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+        
+    }
+};
