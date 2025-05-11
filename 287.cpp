ec5f2d3d7b39128a276a54ce2ee092b8057479da
BakdauletNN <bakdauletrysbekov2007@gmail.com>
2025-05-12 02:30:16 +0600

    Update 287.cpp

--- 287.cpp
+++ 287.cpp
@@ -1,19 +1,23 @@
-from typing import List
-
-class Solution:
-    def findDuplicate(self, nums: List[int]) -> int:
-        slow, fast = 0,0
-        while True:
-            slow = nums[slow]
-            fast = nums[nums[fast]]
-            if slow == fast:
-                break
-        slow2 = 0
-        while True:
-            slow = nums[slow]
-            slow2 = nums[slow2]
-            if slow == slow2:
-                return slow            
-
-
-print(Solution().findDuplicate(nums = [4,1,3,3,2]))
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = 0;
+        int fast = 0;
+        while (true){
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            if (slow == fast){
+                break;
+            }
+    
+    }
+        int slow2 = 0;
+        while (true){
+            slow = nums[slow];
+            slow2 = nums[slow2];
+            if (slow == slow2){
+                return slow;} 
+        }
+        
+    }
+};
