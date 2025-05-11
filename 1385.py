3360533cba6f732cc6de6a275e3b7c892430cd06
BakdauletNN <bakdauletrysbekov2007@gmail.com>
2025-05-11 16:07:04 +0600

    Update 1385.py

--- 1385.py
+++ 1385.py
@@ -1,17 +1,13 @@
-from typing import List
-
 class Solution:
     def findTheDistanceValue(self, arr1: List[int], arr2: List[int], d: int) -> int:
-        left, right = 0, len(arr2) - 1
-        c = 1
-        while True:
-            if abs(arr1[left] - abs(arr2[right])) <= d:
-                return c
-            right -= 1
-            if right == 0:
-                right = len(arr2) - 1
-                left += 1
-                c += 1
-
-print(Solution().findTheDistanceValue(arr1 = [1,4,2,3], arr2 = [-4,-3,6,10,20,30], d = 3
-))
+        count = 0
+        for x in arr1:
+            valid = True
+            for y in arr2:
+                if abs(x - y) <= d:
+                    valid = False
+                    break
+            if valid:
+                count += 1
+        return count
+        
